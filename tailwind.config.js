10904836fbec4b2f64d9398a9195c92a17f72b8d
Dmitry Maslennikov <d.maslennikov@tinkoff.ru>
2024-01-17 13:46:04 +0300

    Initial commit: docusaurus with typescript, eslint, tailwindcss and some css styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {}
+    },
+    darkMode: ['class', '[data-theme="dark"]'],
+    plugins: [
+        require('@tailwindcss/typography')
+    ]
+}
