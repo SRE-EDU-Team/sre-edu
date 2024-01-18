7946e4a66a48bf39f342836c19278356673a2344
Dmitry Maslennikov <d.maslennikov@tinkoff.ru>
2024-01-18 14:42:29 +0300

    Try to fix reveal

--- src/components/reveal.tsx
+++ src/components/reveal.tsx
@@ -1,5 +1,7 @@
+import BrowserOnly from '@docusaurus/BrowserOnly'
 import { type PropsWithChildren, type ReactElement, useEffect, useRef } from 'react'
 import Reveal from 'reveal.js'
+import RevealNotes from 'reveal.js/plugin/notes/notes'
 
 export function RevealSlides ({ children }: PropsWithChildren): ReactElement {
     const ref = useRef(null)
@@ -8,17 +10,22 @@ export function RevealSlides ({ children }: PropsWithChildren): ReactElement {
         if (ref.current === null) return
 
         const r = new Reveal(ref.current, {
-            embedded: true
+            embedded: true,
+            plugins: [RevealNotes]
         })
         void r.initialize()
         return () => { r.destroy() }
     }, [ref])
 
-    return <div className={'w-full h-80'}>
-        <div ref={ref} className={'reveal'}>
-            <div className={'slides'}>
-                {children}
+    return <BrowserOnly>
+        {() => {
+            return <div className={'w-full h-80 not-prose'}>
+                <div ref={ref} className={'reveal'}>
+                    <div className={'slides'}>
+                        {children}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+        }}
+    </BrowserOnly>
 }
